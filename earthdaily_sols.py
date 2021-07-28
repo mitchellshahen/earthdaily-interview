957574bbde73e1153fddeb93c8493fd9460d3f53
mitchellshahen <mitchell.shahen@gmail.com>
2021-07-28 11:44:13 -0400

    Better Commenting and Docs

--- earthdaily_sols.py
+++ earthdaily_sols.py
@@ -4,18 +4,36 @@ Modules to perform the code review tasks for EarthDaily
 
 # ----- # Fibonacci Numbers Task # ----- #
 
-def fib_no_iter(n):
+def fibonacci(n):
+    """
+    A function to calculate the nth Fibonacci number using tail recursion
+
+    :param n: The index of the desired Fibonacci number in the sequence
+    :returns: The nth Fibonacci number
+    """
+
+    # set the initial conditions (0, 1 for the first two numbers)
     if n == 0:
         return 0
     elif n == 1:
         return 1
+    # re-run the function recursively until the first
+    # two Fibonacci numbers are used
     else:
-        return fib_no_iter(n - 1) + fib_no_iter(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 # ----- # List Merge # ----- #
 
 def list_manip(original_list, add_list, delete_list):
     """
+    Function to manipulate a list of strings.
+
+    :param original_list: A list of strings to be manipulated
+    :param add_list: A list of strings to be added to the original
+    :param delete_list: A list of strings to be deleted from the original
+    :returns: A list containing strings from the original list and the
+    added list with no duplications and with strings of the same length
+    ordered in reverse alphabetical order
     """
 
     # add the add list to the original list one element at a time
@@ -58,7 +76,7 @@ def list_manip(original_list, add_list, delete_list):
 
 # ----- # TESTING EACH MODULE # ----- #
 
-final_number = fib_no_iter(n=10)
+final_number = fibonacci(n=10)
 print(final_number)
 
 final_list = list_manip(
